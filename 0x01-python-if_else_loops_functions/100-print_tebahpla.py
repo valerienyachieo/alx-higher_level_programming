78ca1d90d010cdbd406656c33b76e0f6aac0d019
valerienyachieo <valerienyachieo@gmail.com>
2023-06-06 16:49:31 -0700

    reverse alphabet

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        c = chr(i)
+        print("{}".format(c), end='')
+    else:
+        c = chr(i - 32)
+        print("{}".format(c), end='')
