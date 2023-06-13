c6e6c25e432ca6b2f823f4440b59d9cdf2930358
valerienyachieo <valerienyachieo@gmail.com>
2023-06-13 16:19:49 -0700

    number and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{}".format("0 arguments."))
+    elif argc == 1:
+        print("{}".format("1 argument:"))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} {}".format(argc, "arguments"))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
